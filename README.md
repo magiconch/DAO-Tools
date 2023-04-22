a101ce5fc81f378b64913352d9f27b7c0b14e9a9
magiconch <magiconch@outlook.com>
2023-04-23 00:09:56 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # DAO-Tools
-dao工具
+
+## PR 统计
+
+```shell
+
+# 统计 PR 数量并生成 csv 表格
+python3 get_pr.py
+# 计算每个人的 PR 数量
+python3 calc.py
+
+```
+
+
--- get_pr.py
+++ get_pr.py
@@ -1,14 +1,10 @@
+from time import sleep
 import requests
+import json
 import csv
 
 # Set the base URL for the GitHub API
 api_url = 'https://api.github.com'
-
-# Set the name of the user or organization and the repository
-# For example, if the repository is "https://github.com/my-user/my-repo",
-# the user is "my-user" and the repository is "my-repo"
-user = 'AmazingAng'
-repo = 'WTF-Solidity'
 
 # Set your GitHub personal access token
 # This is required to authenticate your request to the GitHub API
@@ -22,38 +18,50 @@ headers = {
     'Accept': 'application/vnd.github+json',
 }
 
-with open("pr.csv","w") as csvfile:
+with open('repos.json', 'r') as f:
+    json_data = json.load(f)
 
-    # Create a CSV writer
-    writer = csv.writer(csvfile)
+    for item in json_data:
+        # Set the name of the user or organization and the repository
+        # For example, if the repository is "https://github.com/my-user/my-repo",
+        # the user is "my-user" and the repository is "my-repo"
+        user = item['username']
+        repo = item['reponame']
 
-    #先写入columns_name
-    writer.writerow(["pr名","用户名","url","合并时间", "奖励数量"])
+        sleep(1)
 
-    for index in range(1, 10000):
-        # Get a list of all the pull requests for the repository
-        prs_url = f'{api_url}/repos/{user}/{repo}/pulls?state=close&page={index}'
-        response = requests.get(prs_url, headers=headers)
-        prs = response.json()
-       
-        writer = csv.writer(csvfile)
+        with open(f"{repo}.csv","w") as csvfile:
 
-        # Write the header row
-        # writer.writerow(['User', 'Pull Request', 'Title'])
+            # Create a CSV writer
+            writer = csv.writer(csvfile)
 
-        # Iterate through the list of pull requests
-        for pr in prs:
-            # Get the login of the user who created the pull request
-            login = pr['user']['login']
+            #先写入columns_name
+            writer.writerow(["pr名","用户名","url","合并时间", "奖励数量"])
 
-            # Get the URL and title of the pull request
-            title = pr['title']
-            url = pr['html_url']
-            merged_at = pr['merged_at'] if pr['merged_at'] else '未合并'
+            for index in range(1, 10000):
+                # Get a list of all the pull requests for the repository
+                prs_url = f'{api_url}/repos/{user}/{repo}/pulls?state=close&page={index}'
+                response = requests.get(prs_url, headers=headers)
+                prs = response.json()
             
+                writer = csv.writer(csvfile)
 
-            # Write the data to the CSV file
-            writer.writerow([title, login, url, merged_at,])
-    
-        if len(prs) < 30:
-            break
+                # Write the header row
+                # writer.writerow(['User', 'Pull Request', 'Title'])
+
+                # Iterate through the list of pull requests
+                for pr in prs:
+                    # Get the login of the user who created the pull request
+                    login = pr['user']['login']
+
+                    # Get the URL and title of the pull request
+                    title = pr['title']
+                    url = pr['html_url']
+                    merged_at = pr['merged_at'] if pr['merged_at'] else '未合并'
+                    
+
+                    # Write the data to the CSV file
+                    writer.writerow([title, login, url, merged_at,])
+            
+                if len(prs) < 30:
+                    break
